7730326e6aba650155b75d49ea06cb47ddabd569
batataverde <batataverdecomsalemanteiga@gmail.com>
2023-05-26 21:25:02 +0900

    Atualização aula 26/05

--- www/js/motor.js
+++ www/js/motor.js
@@ -4,14 +4,34 @@ alert("funcionou!")
 var nome
 var selecao
 var selecaodisciplina
+var selecaodisciplina
 
-function cadastrar() 
+function Cadastrar() 
 {
     nome = document.getElementById('inputnome').value 
     document.getElementById('nomealuno').textContent = nome 
 
     selecao = document.getElementById('disciplina')
-    selecaodisciplina = selecao.options[selecao.SelectIndex].value 
+    selecaodisciplina = selecao.options[selecao.SelectedIndex].value 
+
+    selecao = document.getElementById('mencao')
+    selecaomencao = selecao.options[selecao.SelectedIndex].value 
+
+    localStorage.sertItem(selecaodisciplina, selecaomencao)
+
+    function listar() {
+
+        var exibirmencao
+
+        selecao = document.getElementById('disciplina')
+        selecao = document.getElementById('mencao')
+
+        exibirmencao = localStorage.getItem(selecaodisciplina)
+
+        document.getElementById('exibirdisciplina').text.textContent = selecaodisciplina
+        document.getElementById('exibirmencao').text.textContent = exibirmencao
+        
+    }
 }
 
 
